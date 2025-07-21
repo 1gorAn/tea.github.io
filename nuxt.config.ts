2d9c44b5dc77480f5cd842859d2dfa45b33bf1d1
Анашкин Игорь <anashkinia@dalimo.ru>
2025-07-21 22:06:06 +0400

    nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
   ssr: false, // Отключаем SSR для статического деплоя
   
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/tea.github.io/' : '/',
+    baseURL: '/tea.github.io/', // Для корректной работы в подпапке на GitHub Pages
     buildAssetsDir: '/_nuxt/'
   },
 
